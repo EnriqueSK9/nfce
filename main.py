b2dc6a4e14c8d65abe27016ef161e23808789c8b
Enrique da Silva <enrique.silva@k9n.com.br>
2026-05-22 12:08:31 -0300

    salva qrcodes em uma lista

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from PIL import Image
 from streamlit_qrcode_scanner import qrcode_scanner
 from pyzbar.pyzbar import decode
 
-cam = cv2.VideoCapture(0)
 
+qr_codes = []
+
+cam = cv2.VideoCapture(0)
 if cam.isOpened():
     st.write("Camera available")
 
@@ -13,7 +15,7 @@ if cam.isOpened():
 
     if qr_data:
         st.success("Valid QR Code")
-        st.write(qr_data)
+        qr_codes.append(qr_data)
 
 else:
     st.error("No camera available")
@@ -26,8 +28,13 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file:
-    image = Image.open(uploaded_file)
-
-    st.image(image)
-    st.text(decode(image)[0].data.decode("utf-8"))
-
+    input_images = Image.open(uploaded_file)
+    if input_images:
+        st.success("Valid QR Code")
+        image_list = input_images if isinstance(input_images, list) else [input_images]
+    
+    for i in image_list:
+        decoded = decode(i)[0].data.decode("utf-8")
+        st.write(i)
+        st.text(decoded)
+        qr_codes.append(decoded)
