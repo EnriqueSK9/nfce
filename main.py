aaf800baebf81c74b30802b0bf66cce65b376154
Enrique <enrique.silva@k9n.com.br>
2026-05-23 09:46:38 -0300

    REF: Refiz  tudo

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
-import streamlit as st
-import cv2
+from sys import exit
+from pathlib import Path
 from PIL import Image
-from streamlit_qrcode_scanner import qrcode_scanner
 from pyzbar.pyzbar import decode
+import requests
+from lxml import html
+
+pasta_notas = Path("./notas")
+extensoes_arquivos = [".png",".jpg",".jpeg"]
+imagens = []
+conteudo_qrcodes = []
+
+for arquivo in pasta_notas.glob("*"):
+    if not arquivo.suffix.lower() in extensoes_arquivos:
+        continue
+
+    imagem = Image.open(arquivo)
+    if not imagem:
+        print(f"Aviso: Não foi possivel abrir a imagem {arquivo}")
+        continue
+    imagens.append(imagem)
+
+    decodificado = decode(imagem)
+    if not decodificado:
+        continue
+    if decodificado in conteudo_qrcodes:
+        continue
+    conteudo_qrcodes.append(decodificado[0].data.decode('utf-8'))
+    print(conteudo_qrcodes[-1])
+
+if len(conteudo_qrcodes) == 0:
+    print(f"Erro: Não foi encontrado nenhum qrcode válido na pasta {pasta_notas}")
+    exit()
+
+print(f"QrCodes Encontrados: {len(conteudo_qrcodes)}/{len(imagens)}")
 
 
-qr_codes = []
+xpath_compra = '/html/body/div[1]/div[2]/div/table'
+xpath_total = '/html/body/div[1]/div[2]/div/div[2]'
 
-cam = cv2.VideoCapture(0)
-if cam.isOpened():
-    st.write("Camera available")
-
-    qr_data = qrcode_scanner()
-
-    if qr_data:
-        st.success("Valid QR Code")
-        qr_codes.append(qr_data)
-
-else:
-    st.error("No camera available")
-
-cam.release()
-
-uploaded_file = st.file_uploader(
-    "Upload QR Code Image",
-    type="image"
-)
-
-if uploaded_file:
-    input_images = Image.open(uploaded_file)
-    if input_images:
-        st.success("Valid QR Code")
-        image_list = input_images if isinstance(input_images, list) else [input_images]
+for url in conteudo_qrcodes:
+    response = requests.get(url)
+    response.raise_for_status()
     
-    for i in image_list:
-        decoded = decode(i)[0].data.decode("utf-8")
-        st.write(i)
-        st.text(decoded)
-        qr_codes.append(decoded)
+    tree = html.fromstring(response.content)
+    print(tree.xpath(xpath_compra))
+    print(tree.xpath(xpath_total))
+    print('')
